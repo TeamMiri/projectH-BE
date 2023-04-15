e6bdebfc8f4fc7055ca6596f73029fa6a7da2ab5
youngjinhan <leitmotiv27@gmail.com>
2023-04-15 20:30:14 +0900

    init commit

---
+++ src/test/java/com/teamMiri/projectH/ProjectHApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamMiri.projectH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
