d5bc16ac062c1d315b9ba292273ba57094904352
한영진 <yjhan@miridih.com>
2023-04-18 02:49:08 +0900

    [ADD] user CRUD

--- src/main/java/com/teammiri/projecth/domain/user/controller/UserController.java
+++ src/main/java/com/teammiri/projecth/domain/user/controller/UserController.java
@@ -1,14 +1,18 @@
 package com.teammiri.projecth.domain.user.controller;
 
+import com.teammiri.projecth.domain.user.dto.UserRequestDto;
+import com.teammiri.projecth.domain.user.dto.UserResponseDto;
 import com.teammiri.projecth.domain.user.entity.User;
 import com.teammiri.projecth.domain.user.service.UserService;
 import com.teammiri.projecth.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
@@ -17,11 +21,22 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
+    public List<UserResponseDto> getUsers() {
+        return userService.findUsers();
+    }
+
+    @GetMapping("me")
     public ApiResponse getUser() {
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         User user = userService.getUser(principal.getUsername());
 
         return ApiResponse.success("user", user);
+    }
+
+    @PatchMapping("me")
+    public String update(@PathVariable final String userId, @RequestBody final UserRequestDto userRequestDto) {
+        log.info("update user, userId={}, userRequestDto={}", userId, userRequestDto);
+        return userService.update(userId, userRequestDto);
     }
 }
---
+++ src/main/java/com/teammiri/projecth/domain/user/dto/UserResponseDto.java
@@ -0,0 +1,45 @@
+package com.teammiri.projecth.domain.user.dto;
+
+import com.teammiri.projecth.domain.user.entity.User;
+import com.teammiri.projecth.oauth.entity.ProviderType;
+import com.teammiri.projecth.oauth.entity.RoleType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+public class UserResponseDto {
+    private String userId;
+    private String name;
+    private String email;
+    private String emailVerifiedYn;
+    private Integer age;
+    private String gender;
+    private List<String> techSpec;
+    private String profileImageUrl;
+    private String contactNumber;
+    private String introduction;
+    private ProviderType providerType;
+    private RoleType roleType;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    public UserResponseDto(User user) {
+        this.userId = user.getUserId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.emailVerifiedYn = user.getEmailVerifiedYn();
+        this.age = user.getAge();
+        this.gender = user.getGender();
+        this.techSpec = user.getTechSpec();
+        this.profileImageUrl = user.getProfileImageUrl();
+        this.contactNumber = user.getContactNumber();
+        this.introduction = user.getIntroduction();
+        this.providerType = user.getProviderType();
+        this.roleType = user.getRoleType();
+        this.createdAt = user.getCreatedAt();
+        this.modifiedAt = user.getModifiedAt();
+    }
+}
