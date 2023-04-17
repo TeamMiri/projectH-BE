ce10b863dec6d2858c2c2a8836f5e4f9b328eae0
한영진 <yjhan@miridih.com>
2023-04-17 20:38:58 +0900

    change setting

--- src/main/java/com/teammiri/projecth/domain/project/entity/ProjectStatus.java
+++ src/main/java/com/teammiri/projecth/domain/project/entity/ProjectStatus.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 public enum ProjectStatus {
     OPEN("OPEN", "모집중인 프로젝트"),
     IN_PROGRESS("IN_PROGRESS", "진행중인 프로젝트"),
-    CLOSE("CLOSE", "종료된 프로젝트");
+    CLOSED("CLOSED", "종료된 프로젝트");
 
     private final String text;
     private final String displayName;
